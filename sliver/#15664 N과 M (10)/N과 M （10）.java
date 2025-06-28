fbdaa359279677214ac019cc94681002a2d993e1
오승준 <114849329+HelloSJ00@users.noreply.github.com>
2025-06-28 15:32:39 +0900

    [Silver II 15664] Title: N과 M (10), Time: 140 ms, Memory: 16288 KB -NKLCBHub

---
+++ sliver/#15664 N과 M (10)/N과 M （10）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] visited;
+    static Set<String> set = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        
+        for(int i = 0 ; i<N;i++){
+            visited[i] = true;
+            recur(1,String.valueOf(arr[i]),arr[i]);
+            visited[i] = false;
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    public static void recur(int depth,String s,int prev){
+        if(depth == M && !set.contains(s)){
+            set.add(s);
+            sb.append(s);
+            sb.append("\n");
+        } else {
+            for(int i = 0 ; i < N ; i++){
+                if(!visited[i] && arr[i] >= prev){
+                    visited[i] = true;
+                    recur(depth+1,s + " "+ String.valueOf(arr[i]),arr[i]);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
